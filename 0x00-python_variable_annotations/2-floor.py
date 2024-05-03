4ce424270d2e9d24b63ad6d2579bd33c5bcb4a2d
CyberGA <etomu.joshua@gmail.com>
2024-05-02 17:41:45 -0700

    fix: task 2

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import math
 """
 type-annotated function floor which takes a
 float n as argument and returns the floor of the float.
@@ -10,4 +9,4 @@ def floor(n: float) -> int:
     """
     rounds a number down to the nearest integer.
     """
-    return math.floor(n)
+    return int(n)
